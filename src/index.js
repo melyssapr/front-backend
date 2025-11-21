253817ca5ddb4c936632c08df63234e73bd4331f
Melyssa Pereira <meelyssa2411@gmail.com>
2025-11-21 18:10:55 -0300

    Adicionando o backend

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from "express"
+import cors from "cors"
+
+import { persons } from "./persons.js"
+
+const app = express()
+const port = 3333
+
+// ROTAS: GET, POST, PATCH, PUT, DELETE
+
+app.use(cors())
+
+app.get("/", (request, response) => {
+    response.json(persons)
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}!`)
+})
---
+++ src/persons.js
@@ -0,0 +1,21 @@
+const persons = [
+    {
+        name: "Guilherme Moraes",
+        age: 16,
+        cpf: 12345678900
+    },
+
+    {
+        name: "Pedro Henrique",
+        age: 17,
+        cpf: 98765432110
+    },
+
+    {
+        name: "Melyssa Pereira",
+        age: 16,
+        cpf: 45612378920
+    }
+]
+
+export { persons }
